402df7621ae4fba9f32a5307d51bd2e87e653c46
Kaiohz <goncalves.yohan@gmail.com>
2024-09-29 21:50:17 +0200

    wip supplier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './controllers/health.controller.js';
 import { HealthService } from './services/health.service.js';
+import { PostgreInsertController } from './controllers/postgre.controller.js';
+import { PostgreService } from './services/postgre.service.js';
 
 @Module({
   imports: [],
-  controllers: [HealthController],
-  providers: [HealthService],
+  controllers: [HealthController, PostgreInsertController],
+  providers: [HealthService, PostgreService],
 })
 // eslint-disable-next-line prettier/prettier
 export class AppModule { }
